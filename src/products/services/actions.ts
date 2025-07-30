f6047c982415efb73465ed2268d7710201d30309
4z4c <arturoyz2105@gmail.com>
2025-07-29 23:17:17 -0500

    TQMc2

--- src/products/services/actions.ts
+++ src/products/services/actions.ts
@@ -4,7 +4,28 @@ interface GetProductsOptions {
   filterKey?: string;
 }
 
+const sleep = (seconds: number): Promise<boolean> => {
+  return new Promise( resolve => {
+    setTimeout(() => {
+      resolve( true );
+    }, seconds * 1000);
+  })
+}
+
 export const getProducts = async({ filterKey }: GetProductsOptions): Promise<Product[]> => {
-  const { data } = await productsApi.get<Product[]>('/products');
+
+  sleep(2);
+
+  const fitlerUrl = (filterKey) ? `category=${filterKey}` : '';
+
+  const { data } = await productsApi.get<Product[]>(`/products?${fitlerUrl}`);
+  return data;
+};
+
+export const getProductById = async(id: number): Promise<Product> => {
+
+  // sleep(2);
+
+  const { data } = await productsApi.get<Product>(`/products/${id}`);
   return data;
 };
