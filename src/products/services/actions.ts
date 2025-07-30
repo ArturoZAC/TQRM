898f8b0284115bcd8872579d9a5e889b75f413a0
4z4c <arturoyz2105@gmail.com>
2025-07-29 23:43:52 -0500

    TQMc3

--- src/products/services/actions.ts
+++ src/products/services/actions.ts
@@ -24,7 +24,7 @@ export const getProducts = async({ filterKey }: GetProductsOptions): Promise<Pro
 
 export const getProductById = async(id: number): Promise<Product> => {
 
-  // sleep(2);
+  sleep(5);
 
   const { data } = await productsApi.get<Product>(`/products/${id}`);
   return data;
