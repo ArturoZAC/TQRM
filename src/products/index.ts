afe3f1682d07b64daaf2d2de34d82a5bcd82eddf
4z4c <arturoyz2105@gmail.com>
2025-07-29 21:34:12 -0500

    tqrmc1

---
+++ src/products/index.ts
@@ -0,0 +1,19 @@
+
+export * from './api/products.api';
+export { ProductCard } from './components/ProductCard';
+export { ProductList } from './components/ProductList';
+
+export { useProducts } from './hooks/useProducts';
+
+
+export type * from './interfaces/products';
+
+export { StoreLayout } from './layout/StoreLayout';
+
+
+export { CompleteListPage } from './pages/CompleteListPage';
+export { MensPage } from './pages/MensPage';
+export { NewProduct } from './pages/NewProduct';
+export { WomensPage } from './pages/WomensPage';
+
+export * as productActions from './services/actions'
